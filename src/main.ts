0a71e2635876ae15e196345adefcbbee21ccd331
Ohuo_l <2114845946@qq.com>
2023-06-25 21:20:09 +0800

    feat: 暗黑模式完成

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue'
 //引入element-plus插件与样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+//暗黑模式需要的样式
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 // 忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 //@ts-ignore
